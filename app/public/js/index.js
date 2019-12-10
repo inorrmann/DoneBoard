65a9c45d5c16f28247689018bd51f6cf8ec1ebb9
Robert Parsons <rmpcoding@gmail.com>
2019-12-10 14:07:39 -0800

    fixed css file, added bootstrap to index, signup, and login.html, and deleted an extraneous js file (doneboards.js)

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,27 +1,27 @@
 
 // Hidden by default: login and signin containers
 // ====================================================
-$( "#log-in" ).hide();
-$( "#sign-in" ).hide();
+// $( "#log-in" ).hide();
+// $( "#sign-in" ).hide();
 
 
 // Login button hides main container
 // ====================================================
-$(document).ready(function() {
-  $("#loginButton").click(function(){
-    $(".sign-in-container").slideUp(150);
-    $("#log-in").slideDown(150);
-  });
-});
+// $(document).ready(function() {
+//   $("#loginButton").click(function(){
+//     $(".sign-in-container").slideUp(150);
+//     $("#log-in").slideDown(150);
+//   });
+// });
 
 // Sign in button hides main container
 // ====================================================
-$(document).ready(function() {
-  $("#signinButton").click(function(){
-    $(".sign-in-container").slideUp(150);
-    $("#sign-in").slideDown(150);
-  });
-});
+// $(document).ready(function() {
+//   $("#signinButton").click(function(){
+//     $(".sign-in-container").slideUp(150);
+//     $("#sign-in").slideDown(150);
+//   });
+// });
 
 // Need to add functionality on Sign Up / Submit button to route to projects.html page
 // ====================================================
