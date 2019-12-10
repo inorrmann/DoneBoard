3639c8690bb18036e0996d747ae187f07bcd4d6d
Robert Parsons <rmpcoding@gmail.com>
2019-12-09 17:09:23 -0800

    added jquery functions to index.html

---
+++ app/public/js/index.js
@@ -0,0 +1,36 @@
+
+// Hidden by default: login and signin containers
+// ====================================================
+$( "#log-in" ).hide();
+$( "#sign-in" ).hide();
+
+
+// Login button hides main container
+// ====================================================
+$(document).ready(function() {
+  $("#loginButton").click(function(){
+    $(".sign-in-container").slideUp(150);
+    $("#log-in").slideDown(150);
+  });
+});
+
+// Sign in button hides main container
+// ====================================================
+$(document).ready(function() {
+  $("#signinButton").click(function(){
+    $(".sign-in-container").slideUp(150);
+    $("#sign-in").slideDown(150);
+  });
+});
+
+// Need to add functionality on Sign Up / Submit button to route to projects.html page
+// ====================================================
+
+
+
+// Need to add functionality on Log In / Submit button to route to doneboard.html page
+// ====================================================
+
+
+
+// 
