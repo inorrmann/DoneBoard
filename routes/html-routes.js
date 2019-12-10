02a039a74ee88df5b6ba0ae84ed7f8e25272c1c5
Jordan McQuiston <jordan@jordanmcquiston.com>
2019-12-10 14:48:49 -0800

    creating projects.hbs  and deleted app folder

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,7 @@ const db = require("../models/");
 const router = require("express").Router();
 
 router.get("/projects",function(req, res){
-    return res.render("../public/html/index.html");
+    return res.render("projects");
 })
 
 module.exports = router
