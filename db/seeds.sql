fb386d6788e5b83b459588b615c5643028a53d28
Jordan McQuiston <jordan@jordanmcquiston.com>
2019-12-10 20:38:59 -0800

    Finsihed front end routes connecting to backend routes for creating projects and adding user relationships

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,7 @@ USE doneboard_db;
 
 INSERT INTO Users (first_name, last_name, username, email, phone_number)
 VALUES ("Jordan", "McQuiston", "JordanCley","jordan@thehnrl.com", "7652528980"),
-        ("Brandon", "Thomas", "Brando45","Brando@thehnrl.com", "7658646654");
+        ("Brandon", "Thomas", "Brando45","Brando@thehnrl.com", "7658646654"),
+        ("Noah", "Landon", "NoahsArk","THeARk@thehnrl.com", "7658632454"),
+        ("Kent", "Linder", "Kenny123","Ktest.com", "765834554"),
+        ("Simon", "Mendoza", "SimMendoz","Simon@aol.com", "3124567890");
